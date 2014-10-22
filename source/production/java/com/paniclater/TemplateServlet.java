2397ac844cccab321f9caf0b9c69dcabdb834cb8
Ryan Moore <paniclater@gmail.com>
2014-10-22 14:08:05 -0600

    Added form handling for doGet and doPost

--- source/production/java/com/paniclater/TemplateServlet.java
+++ source/production/java/com/paniclater/TemplateServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class TemplateServlet extends HttpServlet {
-
+	private static final String DEFAULT_NAME = "Guest";
 	@Override
 	public void destroy() {
 		System.out.println("Servlet " + this.getServletName() + " has stopped.");
@@ -22,13 +22,21 @@ public class TemplateServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		resp.getWriter().println("Hello World!");
+		String name;
+		if(req.getParameter("name") == null || req.getParameter("name") == "") {
+			name = DEFAULT_NAME;
+		} else {
+			name = req.getParameter("name");
+		}
+		String html = 	"<!doctype html>";
+		html +=		  	"<html><header><title>Hello " + name + "</title></header>";
+		html +=			"<body><p>Hello " + name + "</p><form action=\"greeting\" method=\"POST\"><input type=\"text\" name=\"name\"><input type=\"submit\" value=\"submit\"></form></body>";
+		resp.getWriter().println(html);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		String name = req.getParameter("name");
-		resp.getWriter().println("Hello " + name + "!");
+		this.doGet(req, resp);
 	}
 	
 }
