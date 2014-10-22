618b910fd52523176ef8f5871a05341ff2e35c92
Ryan Moore <paniclater@gmail.com>
2014-10-22 13:14:11 -0600

    Now with a servlet configured via web.xml

---
+++ source/production/java/com/paniclater/TemplateServlet.java
@@ -0,0 +1,28 @@
+package com.paniclater;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TemplateServlet extends HttpServlet {
+
+	@Override
+	public void destroy() {
+		System.out.println("Servlet " + this.getServletName() + " has stopped.");
+	}
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Servlet " + this.getServletName() + " has started.");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.getWriter().println("Hello World!");
+	}
+	
+}
---
+++ source/test/java/com/paniclater/TemplateServletTest.java
@@ -0,0 +1,5 @@
+package com.paniclater;
+
+public class TemplateServletTest {
+
+}
